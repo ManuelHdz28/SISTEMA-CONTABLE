0c8ed52c908e61f29343bb7abb669e9bdcddb14f
manuel <manuel.hernandez28@outlook.com>
2025-10-29 15:13:24 -0600

    Agregar soporte para múltiples orígenes de confianza CSRF en la configuración

--- CodeSoft/settings.py
+++ CodeSoft/settings.py
@@ -31,7 +31,14 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://sistema-contable.zeabur.app'
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://sistema-contable.zeabur.app',
+    'http://sistema-contable.zeabur.app'
+]
 # Application definition
 
 INSTALLED_APPS = [
